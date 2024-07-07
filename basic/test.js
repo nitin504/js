49f7fd93985ac277ed3a66bd97fe7c5504385272
nitin504 <nitinkumarp504@gmail.com>
2024-07-07 10:33:07 +0000

    symbol

--- basic/test.js
+++ basic/test.js
@@ -1,1 +1,14 @@
+
+//  node basic/test.js
+
 console.log("helllo")
+let a = 12
+const b = 'Nitin'
+var c = '5g'
+//console.table([a,b,c])
+const id = Symbol('123')
+const Id = Symbol('123')
+console.table([id , Id])
+
+
+
